69943caf85ab4206c328ab4e71f37dab3bba2c05
Nguyễn Mạnh Cường <cuongnguyenmanh3008@gmail.com>
2022-05-10 18:34:55 +0700

    a

---
+++ DemoAjax/API/CustomersService.cs
@@ -0,0 +1,54 @@
+﻿using DemoAjax.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAjax.API
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomersService : ControllerBase
+    {
+        Customer customers = new Customer();
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(customers.GetCustomers());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            customers.GetCustomers().Add(customer);
+            return Ok();
+        }
+
+
+        // GET api/<CustomersService>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomersService>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CustomersService>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomersService>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DemoAjax/Models/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAjax.Models
+{
+    public class Customer
+    {
+
+        public Customer()
+        {
+
+        }
+
+        public Customer(string name, string address, int age)
+        {
+            Name = name;
+            Address = address;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+
+
+        public List<Customer> GetCustomers()
+        {
+            return new List<Customer>{ 
+                new Customer("Nguyen Manh Cuong","Bac Ninh",18),
+                new Customer("Nguyen Duc Hung","Hai Duong",20),
+                new Customer("Nguyen Hai Phuong","Bac Ninh",30),
+                new Customer("Nguyen Duc Phap","Hai Phong",22),
+                new Customer("Nguyen Dinh Quang","Ha Noi",26),
+                new Customer("Tran Quoc Bao","Ha Noi",27),
+                new Customer("Nguyen Van Toan","Ha Noi",15),
+                new Customer("Nguyen Van Truong","Hai Duong",16),
+            };
+        }
+    }
+}
